0d92466d9846cc73d611f5282840c8cd89afea8f
Edwin <fc@MacBook.local>
2025-02-18 21:58:49 -0500

    Create class Facturacion

---
+++ src/Facturacion.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Facturacion {
+
+    String cedula;
+    String nombre;
+    public Facturacion(String cedula, String nombre){
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
+
+}
